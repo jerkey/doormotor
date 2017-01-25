945cd0e4184e62b947b9f87ba91e564b1f76cca8
jake waters <jerkey@g-cipher.net>
2017-01-25 13:29:27 -0800

    just taking wiring notes before starting

---
+++ doormotor.ino
@@ -0,0 +1,20 @@
+/*
+ * notes on motor controller board http://spaz.org/~jake/pix/2017-01/20170125_023207.jpg
+middle chip HIP4082
+http://spaz.org/~jake/pix/2017-01/20170125_023259.jpg
+pins 2,3,4,7 are BHI, BLI, ALI, AHI
+2=blue
+3=green
+4=yellow
+7=white
+
+numbers below refer to motor screw terminals on controller
+http://spaz.org/~jake/pix/2017-01/20170125_023751.jpg
+blue high only  = 2 to + = BHI
+green high only = 2 to - = BLI
+
+white high only  = 6 to + = AHI
+yellow high only = 6 to - = ALI
+
+1,3,4,5 all - all the time (during this test of middle chip)
+*/
