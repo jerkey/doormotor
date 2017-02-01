f37006ae66d0592a56932271949f1a7d2f55a4b6
Alex Papazoglou <papazoga@gmail.com>
2017-02-01 00:25:48 -0800

    Bugfixes. Worked!

--- firmware/pwm.c
+++ firmware/pwm.c
@@ -9,8 +9,8 @@ static unsigned char pwm_position;
 
 unsigned char pwm_deadtime = 10;
 unsigned char pwm_step = 1;
-unsigned char pwm_amplitude = 128;
-unsigned char pwm_prescale = 0;
+unsigned char pwm_amplitude = 50;
+unsigned char pwm_prescale = 50;
 char pwm_direction = 1;
 
 static void compute_scaled_sintab(unsigned char mult);
@@ -20,23 +20,8 @@ void pwm_init()
 	DDRD |= (1<<3) | (1<<5) | (1<<6);
 	DDRB |= (1<<1) | (1<<2) | (1<<3);
 	
-	
-	/* Timer 0 */
-	TCCR0A =  (3<<6)	/* SetOC0A on compare match, clear at BOTTOM */
-		| (2<<4)	/* Clear OC0B on compare match, set at BOTTOM */
-		| (1<<0);	/* Phase-correct PWM 0x00 to 0xff */
-
-	/* Timer 1 */
-	TCCR1A =  (3<<6)
-		| (2<<4)
-		| (1<<0);
-
-	/* Timer 2 */
-	TCCR2A =  (3<<6)
-		| (2<<4)
-		| (1<<0);
-
-	pwm_state = 0;
+	PORTD &= ~((1<<3) | (1<<5) | (1<<6));
+	PORTB &= ~((1<<1) | (1<<2) | (1<<3));
 }
 
 static void compute_scaled_sintab(unsigned char mult)
@@ -54,6 +39,23 @@ void pwm_enable()
 {
 	compute_scaled_sintab(pwm_amplitude);
 
+	/* Timer 0 */
+	TCCR0A =  (3<<6)	/* SetOC0A on compare match, clear at BOTTOM */
+		| (2<<4)	/* Clear OC0B on compare match, set at BOTTOM */
+		| (1<<0);	/* Phase-correct PWM 0x00 to 0xff */
+
+	/* Timer 1 */
+	TCCR1A =  (3<<6)
+		| (2<<4)
+		| (1<<0);
+
+	/* Timer 2 */
+	TCCR2A =  (3<<6)
+		| (2<<4)
+		| (1<<0);
+
+
+	pwm_state = 0;
 	TCNT0 = 0;
 	TCNT1H = 0;
 	TCNT1L = 0;
@@ -71,6 +73,13 @@ void pwm_enable()
 
 void pwm_disable()
 {
+	PORTD &= ~((1<<3) | (1<<5) | (1<<6));
+	PORTB &= ~((1<<1) | (1<<2) | (1<<3));
+
+	TCCR0A = 0;
+	TCCR1A = 0;
+	TCCR2A = 0;
+
 	TCCR0B = 0;
 	TCCR1B = 0;
 	TCCR1B = 0;	
@@ -81,19 +90,33 @@ void pwm_disable()
 void pwm_update_phases()
 {
 	unsigned char phase[3];
-	
-	phase[pwm_state] = scaled_sintab[pwm_position];
-	phase[(pwm_state+pwm_direction) % 3] = 0;
-	phase[(pwm_state-pwm_direction) % 3] = scaled_sintab[255-pwm_position];
+	switch (pwm_state) {
+	case 0:
+		phase[0] = scaled_sintab[pwm_position];
+		phase[1] = 0;
+		phase[2] = scaled_sintab[255-pwm_position];
+		break;
+	case 1:
+		phase[1] = scaled_sintab[pwm_position];
+		phase[2] = 0;
+		phase[0] = scaled_sintab[255-pwm_position];
+		break;
+	case 2:
+		phase[2] = scaled_sintab[pwm_position];
+		phase[0] = 0;
+		phase[1] = scaled_sintab[255-pwm_position];
+		break;
+	default:
+		break;
+	}
+	OCR0A = phase[0] + pwm_deadtime;
+	OCR0B = phase[0];
 
-	OCR0A = phase[0];
-	OCR0B = phase[0] + pwm_deadtime;
+	OCR1A = phase[1] + pwm_deadtime;;
+	OCR1B = phase[1];
 
-	OCR1A = phase[1];
-	OCR1B = phase[1] + pwm_deadtime;
-
-	OCR2A = phase[2];
-	OCR2B = phase[2] + pwm_deadtime;
+	OCR2A = phase[2] + pwm_deadtime;;
+	OCR2B = phase[2];
 }
 
 static unsigned char counter = 0;
@@ -111,6 +134,9 @@ ISR(TIMER0_OVF_vect)
 		if (pwm_position < pwm_step) {
 			pwm_state = (pwm_state + 1) % 3;
 			PORTC ^= (1<<5);
+			TCNT0 = 0;
+			TCNT1 = 0;
+			TCNT2 = 0;
 		}
 		counter = 0;
 	}
